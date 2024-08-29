af5d5334069447a51e32f9f8c6db38e150098094
MohakJaiswal <138578818+MohakJaiswal@users.noreply.github.com>
2024-08-29 15:44:18 +0530

    Create TwoSum_Improvised.cpp
    improvised

---
+++ TwoSum_Improvised.cpp
@@ -0,0 +1,28 @@
+/*
+ * @lc app=leetcode id=1 lang=cpp
+ *
+ * [1] Two Sum
+ */
+
+// @lc code=start
+class Solution { //nearly 47 ms
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        int n = nums.size();
+        for (int i = 0 ; i < n ; i++) {
+            //for (int j = 0 ; j < n ; j++) {
+                for (int j = i+1 ; j < n ; j++) {
+                //condition when i and j are equal
+                //if (i == j) {
+                    //continue;
+                //} 
+                if (nums[i] + nums[j] == target) {
+                    return {i , j};
+                }
+            }
+        }
+        return {};
+    }
+};
+// @lc code=end
+
