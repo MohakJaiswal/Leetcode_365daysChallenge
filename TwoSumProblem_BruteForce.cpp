fb81d24407dae18396dbf65fc4731311a50e315e
MohakJaiswal <138578818+MohakJaiswal@users.noreply.github.com>
2024-08-29 15:33:37 +0530

    Create TwoSumProblem_BruteForce.cpp
    #1

---
+++ TwoSumProblem_BruteForce.cpp
@@ -0,0 +1,26 @@
+/*
+ * @lc app=leetcode id=1 lang=cpp
+ *
+ * [1] Two Sum
+ */
+
+// @lc code=start
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        int n = nums.size();
+        for (int i = 0 ; i < n ; i++) {
+            for (int j = 0 ; j < n ; j++) {
+                //condition when i and j are equal
+                if (i == j) {
+                    continue;
+                } if (nums[i] + nums[j] == target) {
+                    return {i , j};
+                }
+            }
+        }
+        return {};
+    }
+};
+// @lc code=end
+
